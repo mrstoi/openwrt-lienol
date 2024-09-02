1703ca10c8ffb9bdfba7502734806a1d5f0d659b
mrstoi <94445586+mrstoi@users.noreply.github.com>
2024-09-02 11:54:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,6 @@ echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.def
 #svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
 #svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-rebootschedule package/luci-app-rebootschedule
 git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
-git clone https://github.com/solarflows/luci-app-qBittorrent-enhanced.git package/luci-app-qBittorrent-enhanced
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon  
 
